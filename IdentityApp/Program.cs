a0a3c5f0f276baab1f5aff5ba0bb2bfaefd161f0
Mikita Luksha <nikita.luksha@softensity.com>
2022-11-10 22:48:57 +0300

    Ch3 IdentityApp setup start programm

--- IdentityApp/Program.cs
+++ IdentityApp/Program.cs
@@ -1,6 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.EntityFrameworkCore;
+using IdentityApp.Models;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AppDataConnection");
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ProductDbContext>(options => options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseStaticFiles();
+app.UseRouting();
+app.UseEndpoints(endpoints => {
+  endpoints.MapDefaultControllerRoute();
+  endpoints.MapRazorPages();
+});
 
 app.Run();
