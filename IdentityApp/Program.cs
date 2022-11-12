683e65515e9bb13ed506766e2f32b1a9b8c4b54b
Mikita Luksha <nikita.luksha@softensity.com>
2022-11-12 11:42:31 +0300

    Ch3 IdentityApp Redirect to https

--- IdentityApp/Program.cs
+++ IdentityApp/Program.cs
@@ -1,18 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using IdentityApp.Models;
 
+// Configure services
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("AppDataConnection");
+
+builder.Services.AddHttpsRedirection(opts => {
+  opts.HttpsPort = 44350;
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ProductDbContext>(options => options.UseSqlServer(connectionString));
 
+// Configure
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
 
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
