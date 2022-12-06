a7fb39abd005a04ef1486c793c49f349553b0c96
Mikita Luksha <nikita.luksha@softensity.com>
2022-12-06 10:41:11 +0300

    Ch7 IdentityApp Seed IdentityUsers

--- IdentityApp/Pages/Identity/Admin/Dashboard.cshtml.cs
+++ IdentityApp/Pages/Identity/Admin/Dashboard.cshtml.cs
@@ -1,12 +1,49 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace IdentityApp.Pages.Identity.Admin
 {
   public class DashboardModel : AdminPageModel
   {
+    private readonly string[] emails = { "alice@example.com", "bob@example.com", "charlie@example.com" };
+
     public int UsersCount { get; set; } = 0;
     public int UsersUnconfirmed { get; set; } = 0;
     public int UsersLockedout { get; set; } = 0;
     public int UsersTwoFactor { get; set; } = 0;
+
+    public UserManager<IdentityUser> UserManager { get; set; }
+
+    public DashboardModel(UserManager<IdentityUser> userMgr)
+    {
+      UserManager = userMgr;
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+      foreach (string email in emails)
+      {
+        var userObject = new IdentityUser
+        {
+          UserName = email,
+          Email = email,
+          EmailConfirmed = true
+        };
+
+        IdentityResult result = await UserManager.CreateAsync(userObject);
+        result.Process(ModelState);
+      }
+
+      if (ModelState.IsValid)
+      {
+        return RedirectToPage();
+      }
+      else
+      {
+        return Page();
+      }
+    }
   }
 }
