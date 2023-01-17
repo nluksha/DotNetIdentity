3084c5f074013bbcd030f1376ca5a4ecf3ddaec2
Mikita Luksha <nikita.luksha@softensity.com>
2023-01-17 21:22:00 +0300

    Ch11 IdentityApp External sign in

--- IdentityApp/Pages/Identity/SignIn.cshtml.cs
+++ IdentityApp/Pages/Identity/SignIn.cshtml.cs
@@ -4,7 +4,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 using Microsoft.AspNetCore.Authorization;
-
+using Microsoft.AspNetCore.Authentication;
+using System.Net;
 
 namespace IdentityApp.Pages.Identity
 {
@@ -26,7 +27,8 @@ namespace IdentityApp.Pages.Identity
     [BindProperty(SupportsGet = true)]
     public string? ReturnUrl { get; set; }
 
-    public SignInModel(SignInManager<IdentityUser> signMgr, UserManager<IdentityUser> usrMgr) {
+    public SignInModel(SignInManager<IdentityUser> signMgr, UserManager<IdentityUser> usrMgr)
+    {
       SignInManager = signMgr;
       UserManager = usrMgr;
     }
@@ -48,7 +50,8 @@ namespace IdentityApp.Pages.Identity
         else if (result.IsNotAllowed)
         {
           var user = await UserManager.FindByEmailAsync(Email);
-          if (user != null && !await UserManager.IsEmailConfirmedAsync(user)) {
+          if (user != null && !await UserManager.IsEmailConfirmedAsync(user))
+          {
             return RedirectToPage("SignUpConfirm");
           }
 
@@ -63,8 +66,41 @@ namespace IdentityApp.Pages.Identity
           TempData["message"] = "Sign In Failed";
         }
       }
-      
+
       return Page();
+    }
+
+    public IActionResult OnPostExternalAsync(string provider)
+    {
+      string callbackUrl = Url.Page("SignIn", "Callback", new { ReturnUrl });
+
+      AuthenticationProperties props = SignInManager.ConfigureExternalAuthenticationProperties(provider, callbackUrl);
+      return new ChallengeResult(provider, props);
+    }
+
+    public async Task<IActionResult> OnGetCallbackAsync()
+    {
+      ExternalLoginInfo info = await SignInManager.GetExternalLoginInfoAsync();
+
+      SignInResult result = await SignInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, true);
+      if (result.Succeeded)
+      {
+        return Redirect(WebUtility.UrlDecode(ReturnUrl ?? "/"));
+      }
+      else if (result.IsLockedOut)
+      {
+        TempData["message"] = "Account Locked";
+      }
+      else if (result.IsNotAllowed)
+      {
+        TempData["message"] = "Sign In Not Allowed";
+      }
+      else
+      {
+        TempData["message"] = "Sign In Failed";
+      }
+
+      return RedirectToPage();
     }
   }
 }
